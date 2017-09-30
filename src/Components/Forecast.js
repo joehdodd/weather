7b78d5f9b9dc88a1aaab7fd3aea0143aef5f1953
Joe Dodd <joehdodd@gmail.com>
2017-09-30 14:08:15 -0400

    add more details to forecast component

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -6,21 +6,17 @@ class Forecast extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      details: {},
       forecast : []
     }
-  }
-
-  SETTINGS = {
-    speed: 1500,
-    slidesToShow: 5,
-    slidesToScroll: 5,
   }
 
   componentDidMount() {
     axios.get(`https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${this.props.match.params.forecastId}%2C%2$%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`)
     .then(res => {
+      let details = res.data.query.results.channel;
       let forecast = res.data.query.results.channel.item.forecast;
-      this.setState({ forecast : forecast });
+      this.setState({ details: details, forecast : forecast });
     })
     .catch( (error) => {
       console.log(error);
@@ -32,32 +28,54 @@ class Forecast extends Component {
     return forecast.map( data => {
       return (
         <div key={data.date} className="forecast-item">
-          <p>{data.date}</p>
-          <p>{data.high}</p>
-          <p>{data.low}</p>
+          <div className="forecast-wrapper">
+            <p>{data.date}</p>
+            <p>{data.high}</p>
+            <p>{data.low}</p>
+          </div>
         </div>
       )
     })
 
   }
 
+  SETTINGS = {
+    speed: 1500,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+  }
+
   render() {
-    let id = this.props.match.params.forecastId;
-    let { forecast } = this.state;
-    console.log(id)
+    let { details, forecast } = this.state;
     return (
-      <div className="slide-container">
-        { forecast === undefined
-            ? <div>
-                <span>Loading Forecast!</span>
-              </div>
-            : <Slider {...this.SETTINGS}>
-                {this.getForecastCard()}
-              </Slider>
+      <div>
+        { !!this.state.details.astronomy
+          ?
+          <div>
+            <div className="detail-container">
+              <h2>Details for {details.location.city}</h2>
+              <p>Sunrise: {details.astronomy.sunrise}</p>
+              <p>Sunset: {details.astronomy.sunset}</p>
+            </div>
+            <div className="slide-container">
+              { forecast === undefined
+                ? <div>
+                  <span>Loading Forecast!</span>
+                </div>
+                : <Slider {...this.SETTINGS}>
+                  {this.getForecastCard()}
+                </Slider>
+              }
+            </div>
+          </div>
+          :
+          <div>
+            <p>Loading!</p>
+          </div>
         }
       </div>
     )
   }
 }
 
-export default Forecast;
+export default Forecast
