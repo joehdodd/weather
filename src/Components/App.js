55fde90183a7ddf35eeb445aa2676fd99a0da22d
Joe <joehdodd@gmail.com>
2017-10-18 14:44:13 -0400

    pass around some data from child => parent => child

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,47 +1,73 @@
 import React from 'react';
 import { CSSTransitionGroup } from 'react-transition-group';
 import { Route } from 'react-router-dom';
+import AddMore from './AddMore';
 import ConditionsList from './ConditionsList';
 import Forecast from './Forecast';
 import '../App.css';
 
-const App = () => (
-  <Route render={({ location }) => (
-    <div>
-      <CSSTransitionGroup
-        transitionName="fade"
-        transitionEnterTimeout={1000}
-        transitionLeaveTimeout={100}
-        transitionAppear={true}
-        transitionLeave={true}
-        transitionAppearTimeout={600}
-      >
-      <Route
-        location={location}
-        key={location.pathname}
-        exact
-        path="/"
-        render={({...props}) => (
-          <ConditionsList {...props}/>
-        )}
-      />
-      </CSSTransitionGroup>
-      <CSSTransitionGroup
-        transitionName="fade"
-        transitionEnterTimeout={200}
-        transitionLeaveTimeout={500}
-      >
-        <Route
-          location={location}
-          key={location.pathname}
-          path={`/forecast/:forecastId`}
-          render={({...props}) => (
-            <Forecast {...props}/>
-          )}
-        />
-      </CSSTransitionGroup>
-    </div>
-  )}/>
-)
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      place: {
+        city: null,
+        state: null
+      }
+    }
+  }
+
+  newPlace = (addMoreData) => {
+    let newCity = addMoreData;
+    this.setState({
+      place: { city: newCity, state: null }
+    })
+  }
+
+  render() {
+    return (
+      <Route render={({ location }) => (
+        <div>
+          <CSSTransitionGroup
+            transitionName="fade"
+            transitionEnterTimeout={1000}
+            transitionLeaveTimeout={100}
+            transitionAppear={true}
+            transitionLeave={true}
+            transitionAppearTimeout={600}
+          >
+          <Route
+            location={location}
+            key={location.pathname}
+            exact
+            path="/"
+            render={({...props}) => (
+              <div>
+                <AddMore addPlace={this.newPlace}/>
+                <ConditionsList newPlace={this.state.place} {...props}/>
+              </div>
+            )}
+          />
+          </CSSTransitionGroup>
+          <CSSTransitionGroup
+            transitionName="fade"
+            transitionEnterTimeout={200}
+            transitionLeaveTimeout={500}
+          >
+            <Route
+              location={location}
+              key={location.pathname}
+              path={`/forecast/:forecastId`}
+              render={({...props}) => (
+                <Forecast {...props}/>
+              )}
+            />
+          </CSSTransitionGroup>
+        </div>
+      )}/>
+    )
+  }
+}
 
 export default App
