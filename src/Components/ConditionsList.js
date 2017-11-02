6f54680277ea4c08535f40be460e87a2da5b692a
Joe <joehdodd@gmail.com>
2017-11-02 16:33:32 -0400

    set state with props, set local storage from state, load local storage and set state

--- src/Components/ConditionsList.js
+++ src/Components/ConditionsList.js
@@ -1,50 +1,40 @@
 import React, { Component } from 'react';
-import Conditions from './Conditions';
+// import Conditions from './Conditions';
 
 class ConditionsList extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      places : [
-        { city: "chattanooga", state: "tn"},
-        { city: "easton", state: "md"},
-        { city: "new york", state: "ny"},
-        { city: "norfolk", state: "va"},
-        { city: "milford", state: "de"},
-        { city: "bangkok", state: null}
-      ],
+      places : []
     }
   }
 
   componentWillMount() {
-    const localStorageRef = localStorage.getItem(`place`);
-
+    let localStorageRef = localStorage.getItem(`places`);
     if (!!localStorageRef) {
-      this.setState( prevState => ({
-        places: [...prevState.places, JSON.parse(localStorageRef)]
-      }))
+      this.setState({
+        places: JSON.parse(localStorageRef)
+      })
     }
-
   }
 
   componentWillReceiveProps(nextProps) {
-    let placesArr = nextProps.places;
-    // console.log(placesArr);
+    let newPlace = nextProps.places;
     this.setState( prevState => ({
-      places: [...prevState.places, nextProps.newPlace]
+      places: [...prevState.places, ...newPlace]
     }));
   }
 
-  componentWillUpdate(nextState) {
-    // console.log(nextState);
-    // localStorage.setItem(`place`, JSON.stringify(nextState.places));
-  }
-
-  render() {
+  componentDidUpdate() {
     let { places } = this.state;
-    const conditionItems =  places.map( (place, id) => {
-        return <Conditions key={id} id={id} city={place.city} state={place.state}/>
-    })
+    localStorage.setItem(`places`, JSON.stringify(places));
+  }
+  
+  render() {
+    // let { places } = this.state;
+    // const conditionItems =  places.map( (place, id) => {
+    //     return <Conditions key={place.id} id={place.id} city={place.id}/>
+    // })
     return (
       <div>
         <div className="conditions-heading">
@@ -52,7 +42,7 @@ class ConditionsList extends Component {
           <p>To see details and a 10-day forecast, tap or click a card.</p>
         </div>
           <div className="weather-container">
-            {conditionItems}
+            {/* {conditionItems} */}
           </div>
       </div>
     )
