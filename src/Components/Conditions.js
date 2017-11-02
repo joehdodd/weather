4b03f970d99f10227ca64094322bdf1610f09d0c
Joe <joehdodd@gmail.com>
2017-11-02 16:34:03 -0400

    logging props, but not rendering, so not sure why im leaving this change

--- src/Components/Conditions.js
+++ src/Components/Conditions.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { Link } from 'react-router-dom';
 import IconSunnyDay from '../images/components/IconSunnyDay';
 import IconPartlyCloudy from '../images/components/IconPartlyCloudy';
@@ -19,14 +19,7 @@ class Conditions extends Component {
   }
 
   componentDidMount() {
-    axios.get(`https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${this.props.city}%2C%2$${this.props.state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`)
-    .then(res => {
-      let data = res.data.query.results.channel;
-      this.setState({ data : data });
-    })
-    .catch( (error) => {
-      console.log(error);
-    })
+    console.log(this.props);
   }
 
   getIcon = (text) => {
