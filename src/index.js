54a8d0e82979276767ac5c858d2fa689d269b6df
Joe Dodd <joehdodd@gmail.com>
2018-02-03 09:53:29 -0500

    redux imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 
