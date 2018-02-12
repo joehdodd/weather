bf7f3ced8c0e026ca39cac50d953fc33255215f6
Joe <joehdodd@gmail.com>
2018-02-12 11:36:43 -0500

    component composition

--- react-ui/src/Components/App.js
+++ react-ui/src/Components/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import { getWeather, removePlace, reorder } from '../redux/actions/actions';
 import { CSSTransitionGroup } from 'react-transition-group';
 import { Route, withRouter } from 'react-router-dom';
 import { DragDropContext } from 'react-beautiful-dnd';
+import Main from './Main';
 import StickyToolbar from './StickyToolbar';
 import GeoContainer from './Geo/GeoContainer';
 import ConditionsList from './Favorites/ConditionsList';
 import Forecast from './Favorites/Forecast';
-// import SearchAutoComplete from './SearchAutoComplete';
 import getAPIWeather from '../utils/apiUtil.js';
 
 const reorderArr = (list, startIndex, endIndex) => {
@@ -18,10 +17,6 @@ const reorderArr = (list, startIndex, endIndex) => {
   result.splice(endIndex, 0, removed);
   return result;
 }
-
-// const rootEl = document.getElementById('root');
-// const modalRoot = document.getElementById('modal');
-
 
 class App extends React.Component {
   constructor(props) {
@@ -99,134 +94,56 @@ class App extends React.Component {
     return dispatch(reorder(reorderedPlaces));
   }
 
-  // showPortal = () => {
-  //   this.setState({ showPortal: true })
-  //   rootEl.classList.add('blur');
-  // }
-  //
-  // hidePortal = (e) => {
-  //   let modalCont = e.target.classList.value === 'modal-cont';
-  //   if (!modalCont) { this.setState({ showPortal: false }) }
-  //   rootEl.classList.remove('blur');
-  // }
-
-
   render() {
     const { places, notFound } = this.props;
     const { data } = this.state;
     return (
       <DragDropContext onDragEnd={this.onDragEnd}>
-      <div id="skew"></div>
-      <StickyToolbar
-        // showPortal={this.showPortal}
-        sendRequest={this.sendRequest}
-        newPlace={this.newPlace}
-      />
-      {/* { this.state.showPortal &&
-        <Portal>
-          <Modal
-            hidePortal={this.hidePortal}
-            sendRequest={this.sendRequest}
-            newPlace={this.newPlace}
-          />
-        </Portal>
-      } */}
-      <Route render={({ location }) => (
-        <div className="wrapper">
-          <div className="container">
-            <CSSTransitionGroup
-              transitionName="fade"
-              transitionEnterTimeout={1000}
-              transitionLeaveTimeout={100}
-              transitionAppear={true}
-              transitionAppearTimeout={600}
-              >
-                <Route
-                  location={location}
-                  key={location.pathname}
-                  exact
-                  path="/"
-                  render={({...props}) => (
-                    <div>
-                      <GeoContainer data={data}/>
-                      { !!places && !notFound &&
-                        <ConditionsList places={places} removeItem={this.removeItem} updateItem={this.updateItem} {...props}/>
-                      }
-                      { notFound &&
-                        <CSSTransitionGroup
-                          transitionName="fade"
-                          transitionAppear={true}
-                          transitionLeave={true}
-                          transitionEnterTimeout={1000}
-                          transitionAppearTimeout={750}
-                          transitionLeaveTimeout={750}
-                          >
-                            <h2>
-                              Oops! Your search returned no results. Check your spelling and try again!
-                              <span style={{fontSize: 48}} role="img" aria-label="confused emoji">🤔</span>
-                            </h2>
-                          </CSSTransitionGroup>
-                        }
-                      </div>
-                    )}
+        <div id="skew"></div>
+        <StickyToolbar
+          sendRequest={this.sendRequest}
+          newPlace={this.newPlace}
+        />
+        <Route render={({ location }) => (
+          <div className="wrapper">
+            <div className="container">
+              <CSSTransitionGroup
+                transitionName="fade"
+                transitionEnterTimeout={1000}
+                transitionLeaveTimeout={100}
+                transitionAppear={true}
+                transitionAppearTimeout={600}
+                >
+                  <Main
+                    location={location}
+                    places={places}
+                    notFound={notFound}
+                    data={data}
+                    removeItem={this.removeItem}
+                    updateItem={this.updateItem}
                   />
                 </CSSTransitionGroup>
                 <CSSTransitionGroup
                   transitionName="fade"
                   transitionEnterTimeout={200}
                   transitionLeaveTimeout={500}
-                  >
-                    <Route
-                      location={location}
-                      key={location.pathname}
-                      path={`/forecast/:forecastId`}
-                      render={({...props}) => (
-                        <Forecast {...props}/>
-                      )}
-                    />
-                  </CSSTransitionGroup>
+                >
+                  <Route
+                    location={location}
+                    key={location.pathname}
+                    path={`/forecast/:forecastId`}
+                    render={({...props}) => (
+                      <Forecast {...props}/>
+                    )}
+                  />
+                </CSSTransitionGroup>
+              </div>
           </div>
-        </div>
-      )}/>
+        )}/>
       </DragDropContext>
     )
   }
 }
-
-// class Portal extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.el = document.createElement('div');
-//   }
-//
-//   componentDidMount() {
-//     modalRoot.appendChild(this.el)
-//   }
-//
-//   componentWillUnmount() {
-//     modalRoot.removeChild(this.el)
-//   }
-//
-//   render() {
-//     return ReactDOM.createPortal(
-//       this.props.children,
-//       this.el
-//     )
-//   }
-// }
-
-// const Modal = (props) => {
-//   return (
-//     <div className="modal">
-//       <div className="modal-cont">
-//         <SearchAutoComplete
-//           sendRequest={props.sendRequest}
-//           hidePortal={props.hidePortal}
-//         />
-//       </div>
-//     </div>
-//   )
-// }
 
 function mapStateToProps(state, ownProps) {
   const {
