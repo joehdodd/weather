c83dd867c67bdd1e4b193a0018242a36e680e9b5
Joe Dodd <joehdodd@gmail.com>
2018-02-21 21:19:31 -0500

    new components and icons

---
+++ react-ui/src/Components/StickyToolbarBottom.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import IconStar from '../images/components/IconStar';
+import IconRefresh from '../images/components/IconRefresh';
+
+const StickyToolbarBottom = props => {
+  return (
+    <div className="bottom-toolbar-container">
+      <div className="favorite-icon" onClick={() => props.toggleFavorites()}>
+        <IconStar iconColor={`${props.isFavorite ? 'orange' : '#3c3c3c'}`} />
+      </div>
+      <div className="refresh-icon" onClick={() => props.refreshWeather()}>
+        <IconRefresh />
+      </div>
+    </div>
+  );
+};
+
+export default StickyToolbarBottom;
