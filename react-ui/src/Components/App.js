fce3837af6fecc2ff57fb0080a6a67c6eddc447f
Joe Dodd <joehdodd@gmail.com>
2018-02-21 21:18:44 -0500

    new methods for toggling favorites and refreshing current data

--- react-ui/src/Components/App.js
+++ react-ui/src/Components/App.js
@@ -5,39 +5,55 @@ import * as actions from '../redux/actions/actions';
 import { Route, withRouter } from 'react-router-dom';
 import { DragDropContext } from 'react-beautiful-dnd';
 import Map from './Map';
-import StickyToolbar from './StickyToolbar';
+import StickyToolbarTop from './StickyToolbarTop';
+import StickyToolbarBottom from './StickyToolbarBottom';
 import Main from './Main';
 import Forecast from './Favorites/Forecast';
 
 class App extends Component {
-
   componentWillReceiveProps(nextProps) {
-    console.log('fire?');
-    const { favorites } = this.props;
-    const favorite = favorites.some(fav => fav.address === nextProps.address)
-    console.log(favorite, actions);
-    actions.isFavorite(favorite)
+    const { isFavorite } = this.props.actions;
+    isFavorite(nextProps.address);
   }
 
-  handleFavorites = () => {
-    console.log('fired handleFavorites');
+  toggleFavorites = () => {
+    const { actions, isFavorite } = this.props;
+    const { removeFavorite, saveFavorite } = actions;
     const params = {
       address: this.props.address,
       lat: this.props.lat,
-      lng: this.props.lng,
-    }
-    return actions.saveFavorite(params);
-  }
+      lng: this.props.lng
+    };
+    isFavorite ? removeFavorite(params) : saveFavorite(params);
+  };
 
-  render () {
-    const { actions, fetching, notFound, address, lat, lng, data, isFavorite } = this.props;
-    console.log(isFavorite);
+  refreshWeather = () => {
+    const { actions, lat, lng } = this.props;
+    const { fetchWeather } = actions;
+    const params = {
+      lat,
+      lng
+    };
+    fetchWeather(params);
+  };
+
+  render() {
+    const {
+      actions,
+      fetching,
+      notFound,
+      address,
+      lat,
+      lng,
+      data,
+      isFavorite
+    } = this.props;
     return (
       <DragDropContext>
         <Map lat={lat} lng={lng} actions={actions} />
         <div className="wrapper">
           <div className="container">
-            <StickyToolbar fetchWeather={actions.fetchWeather} />
+            <StickyToolbarTop fetchWeather={actions.fetchWeather} />
             <Route
               render={({ location }) => (
                 <span>
@@ -59,16 +75,17 @@ class App extends Component {
                 </span>
               )}
             />
-              <div className="bottom-toolbar-container">
-                <span onClick={() => this.handleFavorites()}>Add to favorites</span>
-              </div>
+            <StickyToolbarBottom
+              isFavorite={isFavorite}
+              toggleFavorites={this.toggleFavorites}
+              refreshWeather={this.refreshWeather}
+            />
           </div>
         </div>
       </DragDropContext>
-    )
+    );
   }
-
-};
+}
 
 function mapStateToProps(state, ownProps) {
   const { handleWeather, handleFavorites, router } = state;
@@ -84,7 +101,7 @@ function mapStateToProps(state, ownProps) {
     data,
     isFavorite,
     favorites,
-    location,
+    location
   };
 }
 
