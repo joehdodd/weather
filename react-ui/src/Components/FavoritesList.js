6f86bfb6499b1efad628f31e09225db32e3ea282
Joe <joehdodd@gmail.com>
2018-02-22 15:27:07 -0500

    condition and prompt when use has no favorites

--- react-ui/src/Components/FavoritesList.js
+++ react-ui/src/Components/FavoritesList.js
@@ -2,20 +2,26 @@ import React from 'react';
 import IconStar from '../images/components/IconStar';
 
 const FavoritesList = props => {
-  const fetchFavWeather = async (fav) => {
+  const fetchFavWeather = async fav => {
     const params = {
       lat: fav.lat,
       lng: fav.lng
     };
     await props.fetchWeather(params);
     props.history.push('/');
-  }
+  };
   const favoritesArray = props => {
     return props.favorites.map(fav => {
       return (
         <div className="favorites-list-item" key={fav.lng}>
-          <span className="clickable" onClick={() => fetchFavWeather(fav)}>{fav.address}</span>
-          <div className="favorite-icon" style={{justifySelf: 'end'}} onClick={() => props.toggleFavorites()}>
+          <span className="clickable" onClick={() => fetchFavWeather(fav)}>
+            {fav.address}
+          </span>
+          <div
+            className="favorite-icon"
+            style={{ justifySelf: 'end' }}
+            onClick={() => props.toggleFavorites()}
+          >
             <IconStar iconColor="orange" />
           </div>
         </div>
@@ -23,7 +29,15 @@ const FavoritesList = props => {
     });
   };
 
-  return <div className="favorites-list">{favoritesArray(props)}</div>;
+  return (
+    <div className="favorites-list">
+      {props.favorites.lengt > 0 ? (
+        favoritesArray(props)
+      ) : (
+        <h3 style={{justifySelf: 'center', alginSelf: 'center'}}>You have no favorites! Use the star icon to favorite</h3>
+      )}
+    </div>
+  );
 };
 
 export default FavoritesList;
